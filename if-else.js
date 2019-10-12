6a5db778188d73fb5c4408dbac7709f972b30865
varunbansal644 <varunbansal644@gmail.com>
2019-10-12 19:26:33 +0530

    if..else..if

---
+++ if-else.js
@@ -0,0 +1,16 @@
+// Make a variable x that stores the value of haemoglobin
+var x = 12;
+// Write the if else if chain here
+// Print Low for x less than 14
+// Normal for x in the correct range of 14-17
+// High for x greater than 17
+// Remember to check for equal values too
+if(x<14){
+    console.log("Low");
+}
+else if(x>=14 && x<=17){
+    console.log("Normal");
+}
+else{
+    console.log("High");
+}
