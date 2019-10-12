6022dae9937f42beef01a633ae38f28c97ba6b55
varunbansal644 <varunbansal644@gmail.com>
2019-10-12 19:31:02 +0530

    For LOOp

---
+++ forloop.js
@@ -0,0 +1,11 @@
+// Store the product in a variable named prod
+// Initialise the variable prod as 1 because you want that when it is multiplied to the first //number it retains its identity.
+// Make a for loop here
+// Print the output here
+var num=10;
+var fact=1;
+var i=0;
+for(i=1;i<=num;i++){
+    fact=fact*i;
+}
+console.log(fact);
