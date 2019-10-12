99f53a5187a700340c99611f8995b4757a2a4f6f
varunbansal644 <varunbansal644@gmail.com>
2019-10-12 19:28:09 +0530

    Nested-if.else

---
+++ nested-if.else.js
@@ -0,0 +1,12 @@
+var n1 = 0;
+var n2 = 1;
+var n3 = 4;
+if(n1<n2 && n1<n3){
+    console.log("The smallest number entered is "+n1);
+}
+else if(n2<n1 && n2<n3){
+    console.log("The smallest number entered is "+n2);
+}
+else{
+    console.log("The smallest number entered is "+n3);
+}
